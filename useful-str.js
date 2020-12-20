f27fd414157f51dcc48d5dc7b06bcf5371c14017
velveet <velveet@pm.me>
2020-12-20 07:54:08 -0500

    useful strings

---
+++ useful-str.js
@@ -0,0 +1,7 @@
+
+let greet = 'Hello my dear student.';
+
+console.log(greet.startsWith('Hello'));
+console.log(greet.endsWith('Hello'));
+console.log(greet.includes('dear'));
+console.log(greet.repeat(3));
